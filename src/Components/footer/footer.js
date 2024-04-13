6820bf46e7fca2a1cb1e01d46f62e196e577d721
Karen919 <ka.arakelyan@mail.ru>
2024-04-13 11:05:42 +0400

    origin

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -1,29 +1,27 @@
-
 import React from 'react';
-import { FaEnvelope, FaFacebook, FaInstagram, FaLinkedin, FaPhone } from 'react-icons/fa';
+import { FaAddressBook, FaPhone, FaViber, FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-        <div className='footer_icons'>
-          <p className='footer_icon'><FaFacebook size={30} /></p>
-          <p className='footer_icon'><FaInstagram size={30} /></p>
-          <p className='footer_icon'><FaLinkedin size={30} /></p>
-    </div>
+      <div className='footer_icons'>
+        <p className='footer_icon'><FaViber size={30} /></p>
+        <p className='footer_icon'><FaWhatsapp size={30} /></p>
+      </div>
       <div className="footer-content">
-      <div className="quick_links">
-            <p>Технологии в ваших руках, инженерия в наших.</p>
+        <div className="quick_links">
+          <p>Технологии в ваших руках, инженерия в наших.</p>
         </div>
         <div className="contact_info">
-        <p className='contact_info_email'><FaEnvelope size={15}  /> vkengineering@gmail.com</p>
-          <p className='contact_info_phone'><FaPhone size={15} /> +374(98)-77-77-77</p>
+        <p className='contact_info_phone'><FaPhone size={25} /> +7 961 822 92 04</p>
+          <p className='contact_info_email'><FaAddressBook size={25} /> Владикавказ</p>
+          {/* <p className='contact_info_address'><FaAddressBook size={15} /> Ваш адрес здесь</p> */}
         </div>
       </div>
       {/* <div className="copyright">
         <p>&copy; VKENGINEERING. Все права защищены.</p>
       </div> */}
     </footer>
-    
   );
 };
 
